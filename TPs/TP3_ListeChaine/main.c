68422e85e475281da28cdb69f9d3b86149300b48
hugo <hugo@debianHugo.10.20.172.rev.sfr.net>
2018-11-13 10:17:54 +0100

    Fin menu dans le main

--- TPs/TP3_ListeChaine/main.c
+++ TPs/TP3_ListeChaine/main.c
@@ -9,7 +9,7 @@ int main() {
 	printf("/*-------------------------------*/\n");
 	printf("/*----------*/Menu/*-------------*/\n");
 	printf("/*-------------------------------*/\n");
-
+  T_ListeEtu listeEtu = NULL;
   int isSelected = 0;
   while(1){
     printf("Bienvenue dans le management des étudiants. Veuillez choisir une option : \n\n");
@@ -18,13 +18,13 @@ int main() {
 		printf("3. Supprimer une note\n\n");
 		printf("4. Afficher la liste des étudiants\n\n");
 		printf("5. Afficher le classement des étudiants\n\n");
-		printf("7. Quitter \n\n");
+		printf("6. Quitter \n\n");
     while(isSelected==0){
 		    scanf("%d", &isSelected);
     }
     switch(choix){
-
       case 1: {
+        printf("Ajout d'un étudiant choisi\n");
         int Selection = 1;
         while(Selection){
           int id_etu;
@@ -40,18 +40,51 @@ int main() {
       }
 
       case 2: {
-
-        
+        //Ajouter peut être un test pour voir si les entrées sont valides ???
+        printf("Ajout de note choisi\n");
+        float note;
+  			char* matiere = (char*) malloc(6*sizeof(char));
+  			int id;
+  			afficherListeEtu(listeEtu);
+  			printf("Saisissez l'id de l'étudiant :\n");
+  			scanf("%d", &id);
+  			printf("Saisissez la matière\n");
+  			scanf("%s", matiere);
+  			printf("Saissez la note\n");
+  			scanf("%f", &note);
+  			listeEtu = ajouterNoteEtu(note, matiere, id, listeEtu);
+  			free(matiere);
+  			break;
       }
 
+      case 3: {
+        printf("Suppression de note choisie\n");
+        int id = 0;
+  			char* matiere = (char*) malloc(6*sizeof(char));
+  			afficherListeEtu(listeEtu);
+  			printf("Saisissez l'id de l'étudiant puis la note que vous voulez supprimer ( en entrant la matiere )'\n");
+  			scanf("%d", &id);
+  			scanf("%s", matiere);
+  			listeEtu = supprimerNoteEtu(matiere, id, listeEtu);
+  			break;
+      }
 
+      case 4: {
+        printf("Vous voulez afficher la liste des étudiants : \n");
+        afficherListeEtu(listeEtu);
+        break;
+      }
 
+      case 5: {
+        printf("Vous choisissez d'afficher le classement : \n")
+        afficherClassement(listeEtu);
+        break;
+      }
     default:
       break;
     }
-
   })
-
+  printf("FIn du prograaaaaamme");
 
 
   return 0;
